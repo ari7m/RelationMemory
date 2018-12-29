0477836db606dcd1f39eb6a9ebab78cb4164a8b1
CookieTKHS <200327p@ugs.kochi-tech.ac.jp>
2018-12-29 16:42:02 +0900

    タグ設定の一応完成

--- Setting/Setting_tag.js
+++ Setting/Setting_tag.js
@@ -12,33 +12,43 @@ var num = function() {
 }();
 // 最初に表示される分(title属性の値の数だけ表示)
 window.onload = function() {
-    // const cnt = document.getElementById("tag").title;
-    const id = document.getElementById('tag');
-    const cnt = id.getAttribute('data-cnt');
-    for (var i = 0; i < cnt; i++) {
-        CreateForm();
+    const tag = document.getElementById("tag");
+    const item = document.getElementById("item");
+    const cntTag = tag.getAttribute("data-cntTag");
+    const cntItem = item.getAttribute("data-cntItem");
+    for (var i = 0; i < cntTag; i++) {
+        CreateForm("tag");
         if (i == 0) {
-            id.removeChild(id.firstChild);
-            id.removeChild(id.firstChild);
-            id.removeChild(id.firstChild);
-            id.removeChild(id.lastChild);
+            tag.removeChild(tag.firstChild);
+            tag.removeChild(tag.firstChild);
+            tag.removeChild(tag.firstChild);
+            tag.removeChild(tag.lastChild);
+        }
+    }
+    for (var i = 0; i < cntItem; i++) {
+        CreateForm("item");
+        if (i == 0) {
+            item.removeChild(item.firstChild);
+            item.removeChild(item.firstChild);
+            item.removeChild(item.firstChild);
+            item.removeChild(item.lastChild);
         }
     }
 }
 // form作成する分
-function CreateForm() {
-    const id = document.getElementById('tag');
+function CreateForm(str) {
+    const id = document.getElementById(str);
     const input = document.createElement("input");
     const button = document.createElement("input");
     var N = num();
     // formの属性設定
     input.setAttribute("type", "text");
     input.setAttribute("value", N);
-    input.setAttribute("id", "input" + N);
+    input.setAttribute("id", str + N);
     // buttonの属性設定
     button.setAttribute("type", "button");
     button.setAttribute("value", "削除");
-    button.setAttribute("onclick", "RemoveForm(" + N + ")");
+    button.setAttribute("onclick", "RemoveForm(" + N + ", '" + str + "')");
     button.setAttribute("id", "button" + N);
     // 描画
     id.appendChild(document.createElement("br"));
@@ -47,10 +57,14 @@ function CreateForm() {
     id.appendChild(button);
 }
 // form消す分
-function RemoveForm(a) {
-    const id = document.getElementById("tag");
-    const targ1 = document.getElementById("input" + a);
-    const targ2 = document.getElementById("button" + a);
+function RemoveForm(num, str) {
+    const id = document.getElementById(str);
+    console.log(typeof str);
+    console.log(typeof num);
+    const targ1 = document.getElementById(str + num);
+    console.log(str + num);
+    const targ2 = document.getElementById("button" + num);
+    console.log(targ2);
     const targ3 = targ1.previousElementSibling;
     const targ4 = targ3.previousElementSibling;
     id.removeChild(targ1);
