bbd0f2af1e4da6830ae6b07b04d510638f850322
CookieTKHS <200327p@ugs.kochi-tech.ac.jp>
2019-01-15 16:45:36 +0900

    体験版

--- Start/Create_user.php
+++ Start/Create_user.php
@@ -10,7 +10,7 @@
         $dsn = "mysql:host=localhost; dbname=rmdb; charset=utf8";
         $link = new PDO($dsn, "wolf", "password");
 
-        $q1_1 = $link->query(Select question_1_name from secret_question_1 where id = 1);
+        $q1_1 = $link->query("Select question_1_name from secret_question_1 where id = 1");
         echo $q1_1;
     ?>
 </head>
