4cf86f19287c4133776e4f5c6039e0629e350f9a
Konaien <200358u`ugs.kochi-tech,ac,jp>
2019-01-15 16:10:44 +0900

    ヘッダーの遷移先指定(共有以外)

--- Home/header.js
+++ Home/header.js
@@ -3,3 +3,18 @@ function on_icon(){
     var gear = document.getElementById("setting_icon");
     gear.innerHTML = "a";
 }
+
+function disp(){
+	// 「OK」時の処理開始 ＋ 確認ダイアログの表示
+	if(window.confirm('本当にいいんですね？')){
+		location.href = "example_confirm.html"; // example_confirm.html へジャンプ
+    return true;
+	}
+	// 「OK」時の処理終了
+	// 「キャンセル」時の処理開始
+ else{
+		window.alert('キャンセルされました'); // 警告ダイアログを表示
+    return false;
+	}
+	// 「キャンセル」時の処理終了
+}
