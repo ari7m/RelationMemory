dd49911c95954e44ce6b379005d2261aedbc3942
CookieTKHS <200327p@ugs.kochi-tech.ac.jp>
2019-01-16 10:50:59 +0900

    test

--- Start/Create_check.php
+++ Start/Create_check.php
@@ -26,6 +26,7 @@
                 echo $_POST['name'], '<br /><br />';
                 echo $_POST['ID'], '<br /><br />';
                 $str = $_POST['pwd'];
+                var_dump($str);
                 for ($i = 0, $hid = ''; $i < mb_strlen($str); $i++) {
                     $hid .= '*';
                 }
