2b73bf13ebaf464519db2d0c123a217be24c5c38
CookieTKHS <200327p@ugs.kochi-tech.ac.jp>
2019-01-16 17:17:43 +0900

    test

--- Start/Create_check.php
+++ Start/Create_check.php
@@ -53,11 +53,12 @@
                     for ($i = 1; $i < 4; $i++){
                         $que = 'q'. $i;
                         $sql = 'select question_'. $i. '_name from secret_question_'. $i. ' where question_'. $i. '_id = '. $_POST[$que];
-                        $res = $link -> query($sql);
-                        foreach ($res as $row) {
+                        $result = $link -> query($sql);
+                        var_dump($result);
+                        /*foreach ($result as $row) {
                             $str =  $row['question_'. $i. '_name'];
                         }
-                        echo $str. '<br /><br />';
+                        echo $str. '<br /><br />';*/
                     }
                     /*$sql = 'select question_1_name from secret_question_1 where question_1_id';
                     echo $_POST['q1'], '<br /><br />';
