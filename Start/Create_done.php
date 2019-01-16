4262ab0249fa8c967b06d07dd5c2e3213632a4d9
CookieTKHS <200327p@ugs.kochi-tech.ac.jp>
2019-01-16 18:06:12 +0900

    test

--- Start/Create_done.php
+++ Start/Create_done.php
@@ -22,7 +22,7 @@
         try {
         $dsn = 'mysql:host=localhost; dbname=rmdb; charset=utf8';
         $link = new PDO($dsn, 'root');
-        $sql = 'insert into rmdb.user * values ('. $ID. ', '. $name. ', '. $pwd. ', '. $q1. ', '. $ans1. ', '. $q2. ', '. $ans2. ', '. $q3. ', '. $ans3. ')';
+        $sql = 'insert into rmdb.user values ('. $ID. ', '. $name. ', '. $pwd. ', '. $q1. ', '. $ans1. ', '. $q2. ', '. $ans2. ', '. $q3. ', '. $ans3. ')';
         $do = $link -> query($sql);
     } catch (PDOException $e) {
         echo $e -> getMessage();
