4ed45b4585baefa58858592dc53090eea2f35b2e
CookieTKHS <200327p@ugs.kochi-tech.ac.jp>
2019-01-29 22:50:23 +0900

    done直した

--- Start/Remake_done.php
+++ Start/Remake_done.php
@@ -17,18 +17,9 @@
         $link = new PDO($dsn, 'root');
         $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $link->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-        $sql = 'update rmdb.user set (user_name = :name, user_password = :pwd) where user_id = :ID';
-        $do = $link -> prepare($sql);
-        $param = array(
-            ':name' => $name,
-            ':pwd' => $pwd,
-            ':ID' => $ID,
-        );
-        $do -> execute($param);
-        /*$dsn = 'mysql:host=localhost; dbname=rmdb; charset=utf8';
-        $link = new PDO($dsn, 'root');
-        $sql = 'insert into rmdb.user * values ('. $name. ', '. $ID. ', '. $pwd. ', '. $q1. ', '. $q2. ', '. $q3. ', '. $ans1. ', '. $ans2. ', '. $ans3. ')';
-        $do = $link -> query($sql);*/
+        $sql = 'update rmdb.user set (user_name = "'. $name. '" user_password = "'. $pwd. '")
+                where user_id = "'. $ID. '"';
+        $do = $link -> query($sql);
         http_response_code(301);
         header('Location: ../Template.html');
     ?>
