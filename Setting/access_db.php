33702902f31efec082dcf94158f9b011dffddc4b
CookieTKHS <200327p@ugs.kochi-tech.ac.jp>
2019-01-30 00:41:05 +0900

    accessのやつ直した

--- Setting/access_db.php
+++ Setting/access_db.php
@@ -1,7 +1,7 @@
 <?php
 ini_set( 'display_errors', 1 );
 try {
-	$dbh = new PDO('mysql:host=localhost:8889;dbname=test;charset=utf8','4649','root00');
+	$dbh = new PDO('mysql:host=localhost; dbname=rmdb; charset=utf8','root');
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
