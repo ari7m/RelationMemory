dba3a89e38b0e1b71ad2ecd277a7d16047873674
CookieTKHS <200327p@ugs.kochi-tech.ac.jp>
2019-01-30 01:00:46 +0900

    user_delete(知らないつってんだろ)

--- Setting/user_delete.php
+++ Setting/user_delete.php
@@ -1,47 +1,47 @@
 <head>
-   <?php
-       session_start();
-       $ID = $_SESSION['ID'];
-   ?>
+    <?php
+    session_start();
+    $ID = $_SESSION['ID'];
+    ?>
 </head>
 <?php
-  include "access_db.php"; // データベース取得
-  $sql = "select * from user where user_id = '". $ID. "' "; 
-  $stmt = $dbh -> query($sql);
+include "access_db.php"; // データベース取得
+$sql = "select * from user where user_id = '". $ID. "' ";
+$stmt = $dbh -> query($sql);
 
 foreach ($stmt as $row){
-$user_id = $row["user_id"]; // user_idの確保
-$user_name = $row["user_name"];
-$user_pwd = $row["user_pwd"];
+    $user_id = $row["user_id"]; // user_idの確保
+    $user_name = $row["user_name"];
+    $user_pwd = $row["user_pwd"];
 }
 ?>
 <html>
-  <meta charset="utf-8">
-  <body>
+<meta charset="utf-8">
+<body>
 
-   アカウントの削除を行なった
+    アカウントの削除を行なった
 
-      <?php
+    <?php
 
-      $sql =" DELETE FROM info_b WHERE user_id = '$user_id'";
-      $delete_info_b = $dbh -> query($sql);
+    $sql =" DELETE FROM info_b WHERE user_id = '$user_id'";
+    $delete_info_b = $dbh -> query($sql);
 
-       $sql =" DELETE FROM info_a WHERE user_id = '$user_id'";
-      $delete_info_a = $dbh -> query($sql);
+    $sql =" DELETE FROM info_a WHERE user_id = '$user_id'";
+    $delete_info_a = $dbh -> query($sql);
 
-      $sql =" DELETE FROM tag WHERE user_id ='$user_id'";
-        $delete_tag = $dbh -> query($sql);
-
-
-      $sql =" DELETE FROM user WHERE user_id = '$user_id'";
-      $delete_user = $dbh -> query($sql);
-
-      ?>
-        <script>
-	 location.href = location.href = '../Start/Start.html';
-
-	  </script
+    $sql =" DELETE FROM tag WHERE user_id ='$user_id'";
+    $delete_tag = $dbh -> query($sql);
 
 
-  </body>
+    $sql =" DELETE FROM user WHERE user_id = '$user_id'";
+    $delete_user = $dbh -> query($sql);
+
+    ?>
+    <script>
+    location.href = location.href = '../Start/Start.html';
+
+    </script>
+
+
+</body>
 </html>
