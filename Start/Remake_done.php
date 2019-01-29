6e307ec042066d9187fe77c61f00bfc70ce8cc76
CookieTKHS <200327p@ugs.kochi-tech.ac.jp>
2019-01-29 22:37:18 +0900

    done作った

---
+++ Start/Remake_done.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>
+        アカウント！！！(半ギレ)
+    </title>
+    <meta charset="utf-8" />
+    <?php
+        // SESSIONからデータの受け取り
+        session_start();
+        $name = $_SESSION['name'];
+        $ID = $_SESSION['ID'];
+        $pwd = $_SESSION['pwd'];
+
+        // DBにデータの登録
+        $dsn = 'mysql:host=localhost; dbname=rmdb; charset=utf8';
+        $link = new PDO($dsn, 'root');
+        $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $link->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $sql = 'update rmdb.user set (:name, :pwd) where user_id = :ID';
+        $do = $link -> prepare($sql);
+        $param = array(
+            ':name' => $name,
+            ':pwd' => $pwd,
+            ':ID' => $ID,
+        );
+        $do -> execute($param);
+        /*$dsn = 'mysql:host=localhost; dbname=rmdb; charset=utf8';
+        $link = new PDO($dsn, 'root');
+        $sql = 'insert into rmdb.user * values ('. $name. ', '. $ID. ', '. $pwd. ', '. $q1. ', '. $q2. ', '. $q3. ', '. $ans1. ', '. $ans2. ', '. $ans3. ')';
+        $do = $link -> query($sql);*/
+        http_response_code(301);
+        header('Location: ../Template.html');
+    ?>
+</head>
+<body>
+</body>
+</html>
