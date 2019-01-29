0e6ddcb8959b6faa917a362a9f2f36e5f6f1179c
itoyuki46 <200289i@kochi-tech.ugs.ac.jp>
2019-01-29 22:56:56 +0900

    NULL値を共有する際にでるバグの修正

--- share/receive.php
+++ share/receive.php
@@ -41,12 +41,24 @@ $sql = "select * from share where
        $name = $row["name"];
 $surname_ruby = $row["surname_ruby"];
 $name_ruby = $row["name_ruby"];
-$met_year = $row["met_year"];
-$met_month = $row["met_month"];
-$met_day = $row["met_day"];
-$birth_year = $row["birth_year"];
-$birth_month = $row["birth_month"];
-$birth_day = $row["birth_day"];
+if ($row["met_year"]=="") $met_year = 0;
+ else $met_year = $row["met_year"];
+ 
+ if($row["met_month"]=="") $met_month = 0;
+ else $met_month = $row["met_month"];
+
+if($row["met_day"]=="") $met_day = 0;
+else $met_day = $row["met_day"];
+
+if($row["birth_year"]=="")$birth_year = 0;
+else $birth_year = $row["birth_year"];
+
+if($row["birth_month"]=="")$birth_month = 0;
+else $birth_month = $row["birth_month"];
+
+if($row["birth_day"]=="")$birth_day = 0;
+else $birth_day = $row["birth_day"];
+
 $gender = $row["gender"];
 
 $sql = "INSERT INTO info_a(user_id,manage_id,surname,name,surname_ruby,name_ruby,gender,tag_id,met_year,met_month,met_day,birth_year,birth_month,birth_day) VALUES('$user_id','$cnt','$surname','$name','$surname_ruby','$name_ruby','$gender','0','$met_year','$met_month','$met_day','$birth_year','$birth_month','$birth_day')";
@@ -83,7 +95,7 @@ $result = $num.'個の管理情報受信しました';
     <?php echo $result;   ?></h3>
   <center>
    <br><br>  <br><br>  <br><br>
- <input type = "button" value = "ホームに戻る"  onclick = " location.href = '../Home/Main.php'" id = "button">
+ <input type = "button" value = "ホームに戻る" id = "button">
   </center>
 
 </html>
