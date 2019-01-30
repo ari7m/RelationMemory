f916ecd0c47bc6b6fb8fa229f4975e145b4f36ed
Konaien <200358u`ugs.kochi-tech,ac,jp>
2019-01-31 00:15:52 +0900

    Main.phpの画像表示実装

--- Home/Main.php
+++ Home/Main.php
@@ -2,9 +2,10 @@
     session_start();
     // DBと接続
     include "../Setting/access_db.php";
-    
+    //仮のセッションID
+    $_SESSION['ID'] ='1';
     $id = $_SESSION['ID'];
-    
+
     /* タグが押されたかどうかの判断。押された場合そのタグのidが格納される */
     if(!empty($_POST['Tag'])){
         $tag = $_POST['Tag'];
@@ -12,14 +13,14 @@
     }else{
         $sql = 'SELECT surname, name, tag_id, image FROM info_a WHERE user_id = "' .$id.'" ';
     }
-    
+
     $MI_for_db = $dbh -> query($sql) -> fetchall(PDO::FETCH_ASSOC);
     foreach($MI_for_db as $MI){
         $MI_surname[] = $MI['surname'];
         $MI_name[] = $MI['name'];
         $MI_tag[] = $MI['tag_id'];
         $MI_image[] = $MI['image'];
-        
+
         //$MI_array = count($MI_for_db); // ユーザがもつ管理情報の数を格納
     }
 ?>
@@ -38,7 +39,29 @@
     	<?php for($i = 0; $i < count($MI_for_db); $i++):?>
             <span class ="container">
                 <div class="main">
-                    <image src=<?php $MI_image[$i];?>, class="image">
+                  <?php
+                  $sqll = "SELECT image FROM info_a where manage_id = $i+1";
+                  $stmtl = $dbh->prepare($sqll);
+                  $stmtl->execute();
+                  $row3 = $stmtl->fetch(PDO::FETCH_ASSOC);
+                  $stmtl->execute();
+                  while($row3 = $stmtl->fetch(PDO::FETCH_ASSOC) ){
+                    $DB_PIC_ARRAY[] = $row3['image'];
+                  }
+                  $cnt = 0;
+                  ?>
+                  <div class = "image">
+                  <?php
+                  foreach($DB_PIC_ARRAY as $pic){
+                    $enc_img = base64_encode($pic);
+                    $imginfo = getimagesize('data:application/octet-stream;base64,' . $enc_img);
+                    if($cnt == $i){
+                    echo '<img src="data:' . $imginfo['mime'] . ';base64,' . $enc_img . ' "width=56px" height="56px"  />';
+                  }
+                  $cnt = $cnt + 1;
+                  }
+                  ?>
+                  </div>
                     <button class="button" type="submit" onclick="location.href='Reading.php'">
                     <?php
                         //名前の表示
@@ -46,26 +69,26 @@
                     ?>
                     </button>
                 </div>
-            </span>    
+            </span>
         <?php endfor?>
-		
+
 		<!-- 管理情報追加ボタン -->
-        <button class="create" onclick="location.href='../MIPage/Create_mi.php'">
+        <button class="create" onclick="location.href='./MIPage/Create_mi.php'">
               <img src="plus.png" class="plus">
     	</button>
-		
+
 		<!-- タグの表示部分 -->
 		<?php
-		
+
 		/* タグのID(tag_id)と, タグの名前(tag_name)をとってくる */
 		$sql = 'SELECT tag_name, tag_id FROM tag WHERE user_id = "' .$id. '"';
 		$MI_tag_from_tag = $dbh -> query($sql) -> fetchall(PDO::FETCH_ASSOC);
-		
+
 	    foreach($MI_tag_from_tag as $row){
 			$MI_tag_id[] = $row['tag_id'];
 			$MI_tag_name[] = $row['tag_name'];
 		}
-       
+
 		?>
         <ul class="tag">
             <li id="tag1">
@@ -76,7 +99,7 @@
         			</span>
         		</label>
         	</li>
-        	
+
         	<!-- 全て以外のタグ -->
           <?php for($i = 0; $i < count($MI_tag_name); $i++): ?>
         	<li id="alltag">
